110baade8404b8c6b0924ec817995e7aa5f79ed0
Daniel <danielukoha101@gmail.com>
2023-04-18 15:27:33 +0100

    add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import PickupLinesList from "@/components/PickupLineList";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Finelines</title>
+      </Head>
       <HeroSection />
       <PickupLinesList />
     </>
