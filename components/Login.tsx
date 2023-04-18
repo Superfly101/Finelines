3afa67c370be7ed6ad612c46c1b15e88a939084c
Daniel <danielukoha101@gmail.com>
2023-04-18 11:17:24 +0100

    add login functionality

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,32 +1,68 @@
+import useLogin from "@/hooks/useLogin";
+import { Alert, AlertIcon, AlertDescription } from "@chakra-ui/react";
 import Link from "next/link";
-import { useState } from "react";
-import Button from "./ui/Button";
+import { useEffect, useRef, useState } from "react";
 
 const Login = () => {
-  const [formDate, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-  const handleClick = () => {};
+  const userRef = useRef<HTMLInputElement>(null!);
+  const [password, setPassword] = useState("");
+
+  const { login, error, isLoading } = useLogin();
+
+  const isValidEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    let email = "";
+    let username = "";
+
+    if (isValidEmail(userRef.current.value)) {
+      email = userRef.current.value;
+    } else {
+      username = userRef.current.value;
+    }
+
+    await login(username, email, password);
+  };
 
   return (
     <section className="px-4 pb-16 flex min-h-[calc(100vh-80px)] items-center">
       <div className="w-full border border-secondary p-4 rounded-md max-w-[25rem] mx-auto">
+        {error && (
+          <Alert status="error">
+            <AlertIcon />
+            <AlertDescription>{error}</AlertDescription>
+          </Alert>
+        )}
         <h2 className="my-4 text-center text-secondary font-bold text-xl">
           Login
         </h2>
-        <form className="w-full flex flex-col gap-4">
+        <form className="w-full flex flex-col gap-4" onSubmit={handleSubmit}>
           <div className="form-control">
             <label>Username or Email</label>
-            <input type="text" />
+            <input type="text" ref={userRef} />
           </div>
           <div className="form-control">
             <label>Password</label>
-            <input type="password" placeholder="Password" />
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className="mt-4 form-control">
-            <button className="py-2 px-4 text-white text-sm bg-secondary rounded-full">
+            <button
+              disabled={isLoading}
+              className="py-2 px-4 text-white text-sm bg-secondary rounded-full disabled:opacity-60"
+            >
               Login
             </button>
 
