02427f407ef1799f5e6249e79b1abe7e0016481a
Daniel <danielukoha101@gmail.com>
2023-05-10 21:03:43 +0100

    style: update hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
+import { Button, Heading, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import Button from "./ui/Button";
 
 const HeroSection = () => {
   const router = useRouter();
@@ -10,34 +9,25 @@ const HeroSection = () => {
   };
   return (
     <section className="px-4 pb-8 max-w-[70rem] mx-auto flex flex-col text-center gap-8 min-h-[calc(100vh-80px)] justify-center md: gap-16 md:px-8">
-      {/* <div className="flex justify-center">
-        <Image
-          src="/images/hero-image.png"
-          alt="Landing Page image"
-          width={500}
-          height={500}
-          priority
-        />
-      </div> */}
       <div className="flex flex-col items-center gap-4">
-        <h1 className="text-4xl text-secondary font-bold">
+        <Heading color="blue.400">
           In need of a pick up line to show a bit of your intentions and get
           that conversation started?
-        </h1>
+        </Heading>
 
-        <h2 className="text-secondary text-xl font-[500]">
+        <Heading size="lg">
           We've got you. Be smooth with these funny, clever and corny pick up
           lines.
-        </h2>
+        </Heading>
       </div>
 
-      <h2 className="text-primary font-bold text-3xl">OR,</h2>
+      <h2 className="text-primary font-bold text-3xl">or</h2>
 
       <div className="flex flex-col items-center gap-2">
-        <h2 className="text-xl text-secondary font-[500]">
-          Have you got a pick up line you would like to share?
-        </h2>
-        <Button onClick={handleClick}>Add pick up line</Button>
+        <Text>Have you got a pick up line you would like to share?</Text>
+        <Button onClick={handleClick} variant="outline">
+          Submit pickup line
+        </Button>
       </div>
     </section>
   );
