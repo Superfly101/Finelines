5731e421cd2b30b031d930d7210a047eb5bbbd52
Daniel <danielukoha101@gmail.com>
2023-05-16 00:39:05 +0100

    add like button hover state

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -8,25 +8,25 @@ const HeroSection = () => {
     router.push("/login");
   };
   return (
-    <section className="px-4 pb-8 max-w-[70rem] mx-auto flex flex-col text-center gap-8 min-h-[calc(100vh-80px)] justify-center md: gap-16 md:px-8">
+    <section className="px-4 pb-8 max-w-[70rem] mx-auto flex flex-col text-center gap-8 min-h-[calc(100vh-80px)] justify-center md:gap-16 md:px-8">
       <div className="flex flex-col items-center gap-4">
-        <Heading color="blue.400">
+        <Heading>
           In need of a pick up line to show a bit of your intentions and get
           that conversation started?
         </Heading>
 
-        <Heading size="lg">
+        <Text>
           We've got you. Be smooth with these funny, clever and corny pick up
           lines.
-        </Heading>
+        </Text>
       </div>
 
       <h2 className="text-primary font-bold text-3xl">or</h2>
 
       <div className="flex flex-col items-center gap-2">
-        <Text>Have you got a pick up line you would like to share?</Text>
+        <Text>Have you got a pick up line for us?</Text>
         <Button onClick={handleClick} variant="outline">
-          Submit pickup line
+          Add you
         </Button>
       </div>
     </section>
