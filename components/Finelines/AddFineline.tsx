1abc822dc6e2fba07852df38bc6849a1ac63cb7d
Daniel <danielukoha101@gmail.com>
2023-05-18 21:08:12 +0100

    adding pickup line tags

--- components/Finelines/AddFineline.tsx
+++ components/Finelines/AddFineline.tsx
@@ -1,6 +1,8 @@
+import { SmallCloseIcon } from "@chakra-ui/icons";
 import {
   Button,
   FormControl,
+  FormHelperText,
   FormLabel,
   Input,
   Modal,
@@ -12,6 +14,7 @@ import {
   ModalOverlay,
   Textarea,
 } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 type Prop = {
   isOpen: boolean;
@@ -19,6 +22,19 @@ type Prop = {
 };
 
 const AddFineline = ({ isOpen, onClose }: Prop) => {
+  const [tags, setTags] = useState<string[]>([]);
+  const [tagValue, setTagValue] = useState("");
+
+  const addTag = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setTags((prev) => [...prev, tagValue]);
+    setTagValue("");
+  };
+
+  const removeTag = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const text = event.currentTarget.textContent;
+    setTags((prev) => prev.filter((tag) => tag !== text));
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
@@ -31,12 +47,45 @@ const AddFineline = ({ isOpen, onClose }: Prop) => {
             <Textarea placeholder="Enter pickup line..." rows={5} />
           </FormControl>
           <FormControl>
-            <FormLabel>Tags: </FormLabel>
-            <Input placeholder="Eg. Anime, Dirty, Yoruba" />
+            <FormLabel pos="relative">Tags: </FormLabel>
+            <Input
+              placeholder="Eg. Anime, Dirty, Football"
+              onChange={(e) => setTagValue(e.target.value)}
+              value={tagValue}
+            />
+            <FormHelperText>Maximum of 5 tags</FormHelperText>
+            <Button
+              pos="absolute"
+              right="0"
+              top="8"
+              colorScheme="blue"
+              zIndex="modal"
+              isDisabled={tagValue === "" || tags.length >= 5}
+              onClick={addTag}
+            >
+              Add
+            </Button>
+            <div className="flex py-2 gap-1">
+              {tags.map((tag, index) => (
+                <Button
+                  onClick={removeTag}
+                  key={index}
+                  colorScheme="green"
+                  borderRadius="full"
+                  size="sm"
+                  iconSpacing=".9"
+                  paddingRight="1"
+                  fontWeight="normal"
+                  rightIcon={<SmallCloseIcon />}
+                >
+                  {tag}
+                </Button>
+              ))}
+            </div>
           </FormControl>
         </ModalBody>
         <ModalFooter>
-          <Button mr="3" colorScheme="blue">
+          <Button mr="3" colorScheme="green">
             Submit
           </Button>
           <Button onClick={onClose} colorScheme="red">
