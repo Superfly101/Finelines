c9a61e029e21d1c145bf54f4c711bb7a18d7ab37
Daniel <danielukoha101@gmail.com>
2023-05-20 10:39:39 +0100

    add href to custom toast

--- hooks/useCustomToast.tsx
+++ hooks/useCustomToast.tsx
@@ -1,17 +1,35 @@
-import { useToast } from "@chakra-ui/react";
+import { Alert, AlertTitle, ToastPosition, useToast } from "@chakra-ui/react";
+import Link from "next/link";
 
 type Toast = {
   title?: string;
   status?: "error" | "info" | "warning" | "success" | "loading";
+  position?: ToastPosition;
+  href?: string;
 };
 const useCustomToast = () => {
   const toast = useToast();
   const addToast = (res?: Toast) => {
+    const title = res?.title || "Please sign in to perform this action";
+    const status = res?.status || "info";
+    const position = res?.position || "bottom-left";
+
     toast({
-      title: res?.title || "Please sign in to perform this action",
-      status: res?.status || "error",
+      title,
+      status,
       isClosable: true,
-      position: "bottom-left",
+      position,
+      containerStyle: {
+        marginTop: "4.5rem",
+      },
+      render: () =>
+        res?.href ? (
+          <Link href={res.href}>
+            <Alert status={status} variant="solid">
+              <AlertTitle>{title}</AlertTitle>
+            </Alert>
+          </Link>
+        ) : null,
     });
   };
 
