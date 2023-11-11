090f7e89f0998ae2b93652cb771bc8eb9fb62bfe
Daniel <danielukoha101@gmail.com>
2023-11-11 09:25:30 +0100

    chore: clean unused imports

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,10 +1,6 @@
-import useAuthContext from "@/app/hooks/useAuthContext";
-import useCustomToast from "@/app/hooks/useCustomToast";
 import { Button, Heading, Text } from "@chakra-ui/react";
 
 const HeroSection = ({ onAddFineline }: { onAddFineline: () => void }) => {
-  const { user } = useAuthContext();
-  const { addToast } = useCustomToast();
   return (
     <section className="px-4 pb-8 max-w-[70rem] mx-auto flex flex-col text-center gap-8 min-h-[calc(100vh-80px)] justify-center md:gap-16 md:px-8">
       <div className="flex flex-col items-center gap-4">
