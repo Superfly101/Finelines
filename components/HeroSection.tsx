f2e05394d308800f7afd52e5129a1125ae93a90e
Daniel <danielukoha101@gmail.com>
2023-12-05 21:35:10 +0100

    style: updatej text content

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,27 +5,24 @@ const HeroSection = ({ onAddFineline }: { onAddFineline: () => void }) => {
   return (
     <section className="px-4 pb-8 max-w-[60rem] mx-auto flex flex-col text-center gap-8 min-h-[calc(100vh-80px)] justify-center md:gap-16 md:px-8">
       <div className="flex flex-col items-center gap-4">
-        <Heading>
-          Ready to elevate your social game? Dive into a world of wit and humor
-          with our curated collection of conversation starters and pickup lines.
-        </Heading>
+        <Heading>Ready to elevate your social game? </Heading>
         <Text>
           Whether you&apos;re aiming for a good laugh or a genuine connection,
+          with our curated collection of conversation starters and pickup lines
           we&apos;ve got you covered.
         </Text>
       </div>
 
       <div className="flex flex-col items-center gap-2">
         <Text fontWeight="semibold" fontSize="3xl">
-          Or
+          Rather
         </Text>
         <Text fontSize="lg">
           Have a killer line up your sleeve? Share the love! Submit your own
-          conversation starter or pickup line, and our discerning admin team
-          will give it the nod of approval.
+          lines, and our discerning admin team will give it the nod of approval.
         </Text>
         <Button onClick={onAddFineline} variant="outline" colorScheme="blue">
-          Submit for review
+          Submit lines for review
         </Button>
       </div>
 
