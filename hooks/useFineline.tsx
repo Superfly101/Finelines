caea89a90bbfaf6ec55bb965c85d1040047b75aa
Daniel <danielukoha101@gmail.com>
2023-12-05 21:02:40 +0100

    feat: add abort controller signal to request

--- hooks/useFineline.tsx
+++ hooks/useFineline.tsx
@@ -1,19 +1,27 @@
 import { useCallback, useEffect, useState } from "react";
 import { apiUrl } from "../constants";
-import { PickupLine } from "../types/pickupLine";
+import { Fineline } from "../types/Fineline";
 import useCustomToast from "./useCustomToast";
 
 type Config = {
-  url: string;
+  status?: string;
+  url?: string;
   method?: "POST" | "GET" | "PUT" | "DELETE" | "PATCH";
   headers?: HeadersInit;
   body?: BodyInit;
 };
 
-const useFineline = () => {
+const useFineline = ({
+  status = "approved",
+  method = "GET",
+  headers,
+  body,
+}: Config) => {
   const [isLoading, setIsLoading] = useState<boolean>();
   const [error, setError] = useState<string | null>(null);
-  const [finelines, setFinelines] = useState<PickupLine[]>([]);
+  const [finelines, setFinelines] = useState<Fineline[]>([]);
+  const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(false);
   const { addToast } = useCustomToast();
 
   const sendRequest = useCallback(
@@ -42,7 +50,7 @@ const useFineline = () => {
           return;
         }
 
-        setFinelines(data);
+        setFinelines(data.finelines);
         setIsLoading(false);
         return data;
       } catch (err) {
@@ -57,6 +65,59 @@ const useFineline = () => {
     },
     []
   );
+
+  useEffect(() => {
+    const getFinelines = async (signal: AbortSignal) => {
+      try {
+        setError(null);
+        setIsLoading(true);
+
+        const res = await fetch(
+          `${apiUrl}/pickup-lines?status=${status}&page=${page}`,
+          {
+            method: method,
+            headers: headers ? headers : {},
+            body: body ? body : null,
+            signal: signal,
+          }
+        );
+
+        const data = await res.json();
+
+        if (!res.ok) {
+          console.log(data);
+          setError(data.message);
+          setIsLoading(false);
+          addToast({
+            status: "error",
+            title: "An error occured, please try again later.",
+            position: "bottom-left",
+          });
+          return;
+        }
+
+        setFinelines((prev) => [...prev, ...data.finelines]);
+        setIsLoading(false);
+        setHasNextPage(data.hasNextPage);
+      } catch (err) {
+        if ((err as Error).name === "AbortError") return;
+        console.log(err);
+        addToast({
+          status: "error",
+          title: "An error occured, please try again later.",
+          position: "bottom-left",
+        });
+        setIsLoading(false);
+      }
+    };
+
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    getFinelines(signal);
+
+    return () => controller.abort();
+  }, []);
 
   return { isLoading, error, finelines, setFinelines, sendRequest };
 };
