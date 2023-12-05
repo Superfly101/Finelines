839f5a9fdc63555618358e09cc5682493d53c1d7
Daniel <danielukoha101@gmail.com>
2023-12-06 00:36:10 +0100

    feat: infinite scroll to pending finelines

--- components/finelines/Pending.tsx
+++ components/finelines/Pending.tsx
@@ -4,126 +4,137 @@ import { Fineline } from "@/types/Fineline";
 import { CheckIcon } from "@chakra-ui/icons";
 import { Avatar, Text, useDisclosure } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
+import React from "react";
 import { Dispatch, SetStateAction, useState } from "react";
 import MyButton from "../ui/Button";
 import CustomModal from "../ui/CustomModal";
 
 type Props = Fineline & {
-  updateFinelines: Dispatch<SetStateAction<Fineline[] | null>>;
+  updateFinelines: Dispatch<SetStateAction<Fineline[]>>;
 };
 
-const Pending = ({ _id, user, text, tags, status, updateFinelines }: Props) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [action, setAction] = useState<string | null>(null);
+const PendingItem = React.forwardRef<HTMLLIElement, Props>(
+  ({ _id, user, text, tags, status, updateFinelines }, ref) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [action, setAction] = useState<string | null>(null);
 
-  const { data: session } = useSession();
-  const { isLoading, error, finelines, setFinelines, sendRequest } =
-    useFineline();
-  const [isApprovingFineline, setIsApprovingFineline] = useState(false);
-  const [isRejectingFineline, setIsRejectingFineline] = useState(false);
+    const { data: session } = useSession();
+    const { isLoading } = useFineline();
+    const [isApprovingFineline, setIsApprovingFineline] = useState(false);
+    const [isRejectingFineline, setIsRejectingFineline] = useState(false);
 
-  const updatePickupline = async (action: string) => {
-    if (session?.user) {
-      await sendRequest({
-        url: `pickup-lines/${_id}`,
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${session.user.token}`,
-        },
-        body: JSON.stringify({ status: action }),
-      });
-    }
-    setIsApprovingFineline(false);
-    setIsRejectingFineline(false);
-    updateFinelines((current) =>
-      current ? current.filter((fineline) => fineline._id !== _id) : null
-    );
-  };
+    const updatePickupline = async (action: string) => {
+      if (session?.user) {
+        const res = await fetch(`${apiUrl}/pickup-lines/${_id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${session.user.token}`,
+          },
+          body: JSON.stringify({ status: action }),
+        });
 
-  const handleReject = async () => {
-    setAction("Reject");
-    onOpen();
-  };
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data);
+          return;
+        }
+      }
 
-  const handleApprove = () => {
-    setAction("Approve");
-    onOpen();
-  };
+      setIsApprovingFineline(false);
+      setIsRejectingFineline(false);
+      updateFinelines((current) => [
+        ...current.filter((fineline) => fineline._id !== _id),
+      ]);
+    };
 
-  const handleAction = () => {
-    onClose();
-    if (action === "Approve") {
-      setIsApprovingFineline(true);
-      updatePickupline("approved");
-    } else if (action === "Reject") {
-      setIsRejectingFineline(true);
-      updatePickupline("rejected");
-    }
-  };
+    const handleReject = async () => {
+      setAction("Reject");
+      onOpen();
+    };
 
-  return (
-    <>
-      <CustomModal
-        onClose={onClose}
-        isOpen={isOpen}
-        title={`Confirm ${action}`}
-        body={"Are you sure you want to approve this pickup line?"}
-        secondaryAction={action}
-        handleClick={handleAction}
-      />
-      <li
-        className={`flex flex-col w-full max-w-[40rem] mx-auto p-4 border rounded-lg drop-shadow-xl`}
-      >
-        <section>
-          <div className="relative flex gap-2 items-center">
-            <Avatar />
-            <Text className="font-semibold">{user}</Text>
-          </div>
-          <Text className="py-2">{text}</Text>
-          {tags && (
-            <small className="flex gap-1">
-              Tags:
-              <div className="flex gap-2">
-                {tags.map((tag, index) => (
-                  <p key={index}>{tag}</p>
-                ))}
-              </div>
-            </small>
-          )}
-          <div className="flex gap-4 pt-4">
-            {session?.user?.role == "admin" ? (
-              <>
-                <MyButton
-                  color="blue"
-                  rightIcon={<CheckIcon />}
-                  className="w-full"
-                  onClick={handleApprove}
-                  isLoading={isLoading && isApprovingFineline}
-                  isDisabled={isLoading && isApprovingFineline}
-                  loadingText="Approving..."
-                >
-                  Approve
-                </MyButton>
-                <MyButton
-                  color="red"
-                  className="w-full"
-                  onClick={handleReject}
-                  isLoading={isLoading && isRejectingFineline}
-                  isDisabled={isLoading && isRejectingFineline}
-                  loadingText="Rejecting..."
-                >
-                  Reject
-                </MyButton>
-              </>
-            ) : (
-              <p>Status: {status}</p>
+    const handleApprove = () => {
+      setAction("Approve");
+      onOpen();
+    };
+
+    const handleAction = () => {
+      onClose();
+      if (action === "Approve") {
+        setIsApprovingFineline(true);
+        updatePickupline("approved");
+      } else if (action === "Reject") {
+        setIsRejectingFineline(true);
+        updatePickupline("rejected");
+      }
+    };
+
+    return (
+      <>
+        <CustomModal
+          onClose={onClose}
+          isOpen={isOpen}
+          title={`Confirm ${action}`}
+          body={"Are you sure you want to approve this pickup line?"}
+          secondaryAction={action}
+          handleClick={handleAction}
+        />
+        <li
+          className={`flex flex-col w-full max-w-[40rem] mx-auto p-4 border rounded-lg drop-shadow-xl`}
+          ref={ref ? ref : null}
+        >
+          <section>
+            <div className="relative flex gap-2 items-center">
+              <Avatar />
+              <Text className="font-semibold">{user}</Text>
+            </div>
+            <Text className="py-2">{text}</Text>
+            {tags && (
+              <small className="flex gap-1">
+                Tags:
+                <div className="flex gap-2">
+                  {tags.map((tag, index) => (
+                    <p key={index}>{tag}</p>
+                  ))}
+                </div>
+              </small>
             )}
-          </div>
-        </section>
-      </li>
-    </>
-  );
-};
+            <div className="flex gap-4 pt-4">
+              {session?.user?.role == "admin" ? (
+                <>
+                  <MyButton
+                    color="blue"
+                    rightIcon={<CheckIcon />}
+                    className="w-full"
+                    onClick={handleApprove}
+                    isLoading={isLoading && isApprovingFineline}
+                    isDisabled={isLoading && isApprovingFineline}
+                    loadingText="Approving..."
+                  >
+                    Approve
+                  </MyButton>
+                  <MyButton
+                    color="red"
+                    className="w-full"
+                    onClick={handleReject}
+                    isLoading={isLoading && isRejectingFineline}
+                    isDisabled={isLoading && isRejectingFineline}
+                    loadingText="Rejecting..."
+                  >
+                    Reject
+                  </MyButton>
+                </>
+              ) : (
+                <p>Status: {status}</p>
+              )}
+            </div>
+          </section>
+        </li>
+      </>
+    );
+  }
+);
 
-export default Pending;
+PendingItem.displayName = "PendingItem";
+
+export default PendingItem;
--- components/finelines/PendingList.tsx
+++ components/finelines/PendingList.tsx
@@ -3,43 +3,80 @@
 import { Text } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import Pending from "./Pending";
 import LoadingSpinner from "../ui/LoadingSpinner";
-import useFineline from "@/hooks/useFineline";
-import { Fineline } from "@/types/Fineline";
+import { Fineline, FinelinesResponse } from "@/types/Fineline";
+import { apiUrl } from "@/constants";
 
 const PendingList = () => {
   const { data: session } = useSession();
-  const {
-    finelines,
-    isLoading,
-    error,
-    sendRequest: fetchFinelines,
-  } = useFineline();
-  const [pendingFinelines, setPendingFinelines] = useState<Fineline[] | null>(
-    null
-  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [pendingFinelines, setPendingFinelines] = useState<Fineline[]>([]);
+  const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(false);
+  const observer = useRef<IntersectionObserver | null>(null);
 
   useEffect(() => {
-    try {
-      if (session?.user.role === "user") {
-        fetchFinelines({
-          url: `pickup-lines?status=pending&user=${session?.user.username}`,
-        });
-      } else if (session?.user.role === "admin") {
-        fetchFinelines({
-          url: `pickup-lines?status=pending`,
-        });
+    const fetchPendingFinelines = async (signal: AbortSignal) => {
+      setIsLoading(true);
+      try {
+        if (!session?.user.username) return;
+        const res = await fetch(
+          `${apiUrl}/pickup-lines?status=pending&pageSize=50${
+            session?.user.role !== "admin"
+              ? `&user=${session?.user.username}`
+              : ""
+          }`,
+          { signal: signal }
+        );
+
+        const data = await res?.json();
+
+        if (!res?.ok) {
+          throw new Error(data.message);
+        }
+
+        if (data) {
+          setPendingFinelines((prev) => [
+            ...prev,
+            ...(data as FinelinesResponse).finelines,
+          ]);
+          setHasNextPage((data as FinelinesResponse).hasNextPage);
+        }
+
+        setIsLoading(false);
+      } catch (error) {
+        if (signal.aborted) return;
+        console.log(error);
+        setIsLoading(false);
+        setHasNextPage(false);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  }, [session?.user, fetchFinelines]);
+    };
 
-  useEffect(() => {
-    setPendingFinelines(finelines);
-  }, [finelines]);
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    fetchPendingFinelines(signal);
+
+    return () => controller.abort();
+  }, [session?.user.role, session?.user.username]);
+
+  const lastPendingFineline = useCallback(
+    (node: HTMLLIElement) => {
+      if (isLoading) return;
+
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [isLoading, hasNextPage]
+  );
 
   return (
     <section className="px-4 py-">
@@ -48,13 +85,26 @@ const PendingList = () => {
       ) : (
         <ul className="flex flex-col gap-4">
           {pendingFinelines?.length ? (
-            pendingFinelines.map((fineline) => (
-              <Pending
-                key={fineline._id}
-                {...fineline}
-                updateFinelines={setPendingFinelines}
-              />
-            ))
+            pendingFinelines.map((fineline, index) => {
+              if (pendingFinelines.length === index + 1) {
+                return (
+                  <Pending
+                    ref={lastPendingFineline}
+                    key={fineline._id}
+                    {...fineline}
+                    updateFinelines={setPendingFinelines}
+                  />
+                );
+              }
+
+              return (
+                <Pending
+                  key={fineline._id}
+                  {...fineline}
+                  updateFinelines={setPendingFinelines}
+                />
+              );
+            })
           ) : (
             <div className="flex flex-col gap-4 justify-center items-center h-96">
               <Text className="font-semibold text-xl">
